9e0ae452cbbc1553c1297a21f802feded9f06d0e
Adel Ayman <85571327+adelayman1@users.noreply.github.com>
2023-06-07 07:47:03 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -17,6 +17,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
       <meta property="og:title" content="adelayman1" />
       <meta property="og:description" content="FaceTime clone api" />
       <link rel="icon" href="https://i.ibb.co/SdKf7W8/d16x16.png" sizes='16x16'/>
